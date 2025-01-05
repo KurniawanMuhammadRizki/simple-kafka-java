e176ce765eca117e9e39c45c2ee107092c741434
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2025-01-05 20:31:22 +0700

    initial commit

---
+++ src/test/java/kurniawan_rizki/simple_kafka_java/SimpleKafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package kurniawan_rizki.simple_kafka_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
